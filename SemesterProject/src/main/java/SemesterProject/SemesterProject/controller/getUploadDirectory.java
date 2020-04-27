5bb51405bd157cc7bac7ab2c7e587fa9787340eb
GeeWilliikers <61354840+GeeWilliikers@users.noreply.github.com>
2020-04-27 17:32:09 -0500

    testing cole

---
+++ SemesterProject/src/main/java/SemesterProject/SemesterProject/controller/getUploadDirectory.java
@@ -0,0 +1,8 @@
+package SemesterProject.SemesterProject.controller;
+
+public class getUploadDirectory {
+	String uploadDirectory = "C:\\Users\\cole\\Desktop\\SoftwareEngineeringProject-master\\SemesterProject\\src\\main\\webapp\\WEB-INF\\views";
+	public getUploadDirectory() {
+		
+	}
+}
